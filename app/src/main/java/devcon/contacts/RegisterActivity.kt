b93c162f7c702f48a2854b51bf69e7e70356a438
Kim-deokhyeon <kkkddh9965@daum.net>
2025-03-13 01:29:47 +0900

    [Feat] 연락처 추가 화면 추가 - 각 버튼별 동작 추가

--- app/src/main/java/devcon/contacts/RegisterActivity.kt
+++ app/src/main/java/devcon/contacts/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import devcon.learn.contacts.R
 
@@ -14,15 +15,43 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_resgister)
 
         val btnMore = findViewById<Button>(R.id.btnMore)
-        val editText4 = findViewById<EditText>(R.id.textInputBirthday)
-        val editText5 = findViewById<EditText>(R.id.textInputGender)
-        val editText6 = findViewById<EditText>(R.id.textInputMemo)
+        val btnCancel = findViewById<Button>(R.id.btnCancel)
+        val btnSave = findViewById<Button>(R.id.btnSave)
+
+        // 추후 2단계 과제를 위해 사용 예정
+        val textInputName = findViewById<EditText>(R.id.textInputName)
+        val textInputPhone = findViewById<EditText>(R.id.textInputPhone)
+        val textInputMail = findViewById<EditText>(R.id.textInputMail)
+        val textInputBirthday = findViewById<EditText>(R.id.textInputBirthday)
+        val textInputGender = findViewById<EditText>(R.id.textInputGender)
+        val textInputMemo = findViewById<EditText>(R.id.textInputMemo)
 
         btnMore.setOnClickListener {
-            editText4.visibility = View.VISIBLE
-            editText5.visibility = View.VISIBLE
-            editText6.visibility = View.VISIBLE
+            textInputBirthday.visibility = View.VISIBLE
+            textInputGender.visibility = View.VISIBLE
+            textInputMemo.visibility = View.VISIBLE
             btnMore.visibility = View.GONE
+        }
+
+        btnCancel.setOnClickListener {
+            val cancelToast = Toast.makeText(this.applicationContext, "취소 되었습니다.", Toast.LENGTH_SHORT)
+            cancelToast.show()
+            finish()
+        }
+
+        btnSave.setOnClickListener {
+            val name = textInputName.text
+            val phoneNumber = textInputPhone.text
+
+            if (name.isEmpty() || phoneNumber.isEmpty()) {
+                val missingFieldToast = Toast.makeText(this.applicationContext, "이름과 전화번호는 필수입니다.", Toast.LENGTH_SHORT)
+                missingFieldToast.show()
+                return@setOnClickListener
+            }
+
+            val successSaveToast = Toast.makeText(this.applicationContext, "저장이 완료 되었습니다.", Toast.LENGTH_SHORT)
+            successSaveToast.show()
+            finish()
         }
     }
 }
