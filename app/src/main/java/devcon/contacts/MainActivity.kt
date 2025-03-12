13cc1ce98fc1f09608bbb4ea3fc2d86fc21f4bb5
Kim-deokhyeon <kkkddh9965@daum.net>
2025-03-13 00:40:40 +0900

    [feat] 연락처 추가 화면 추가 - 화면 이동이 가능하도록 추가

--- app/src/main/java/devcon/contacts/MainActivity.kt
+++ app/src/main/java/devcon/contacts/MainActivity.kt
@@ -1,12 +1,26 @@
 package devcon.contacts
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import devcon.learn.contacts.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMore = findViewById<Button>(R.id.btnAddContact)
+
+        btnMore.setOnClickListener {
+            val intent = Intent(
+                this@MainActivity,
+                RegisterActivity::class.java
+            )
+
+            startActivity(intent)
+        }
     }
 }
