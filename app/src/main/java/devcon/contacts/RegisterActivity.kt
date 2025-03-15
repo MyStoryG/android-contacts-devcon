5b7dfe10626daf2c0767086e385fd97e4f851406
Kim-deokhyeon <kkkddh9965@daum.net>
2025-03-16 04:17:46 +0900

    [Feat] 연락처 저장 나가기 처리 - 작성중인 값이 있을 경우, 다이얼로그 노출 처리

--- app/src/main/java/devcon/contacts/RegisterActivity.kt
+++ app/src/main/java/devcon/contacts/RegisterActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.RadioGroup
 import android.widget.Toast
 import devcon.core.BaseActivity
 import devcon.domain.Contact
@@ -19,7 +20,11 @@ class RegisterActivity : BaseActivity() {
 
     private lateinit var textInputName: EditText
     private lateinit var textInputPhone: EditText
+    private lateinit var textInputMail: EditText
     private lateinit var additionalDataLayout: LinearLayout
+    private lateinit var textInputBirthday: EditText
+    private lateinit var genderRadioGroup: RadioGroup
+    private lateinit var textInputMemo: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +40,11 @@ class RegisterActivity : BaseActivity() {
 
         textInputName = findViewById(R.id.textInputName)
         textInputPhone = findViewById(R.id.textInputPhone)
+        textInputMail = findViewById(R.id.textInputMail)
+        textInputBirthday = findViewById(R.id.textInputBirthday)
+        genderRadioGroup = findViewById(R.id.genderRadioGroup)
+        textInputMemo = findViewById(R.id.textInputMemo)
+
         additionalDataLayout = findViewById(R.id.additionalDataLayout)
 
         listOf(buttonMore, buttonCancel, buttonSave).forEach { it.setOnClickListener(this) }
@@ -48,10 +58,17 @@ class RegisterActivity : BaseActivity() {
             }
 
             R.id.buttonCancel -> {
-                val cancelToast = Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT)
-                cancelToast.show()
-                setResult(Activity.RESULT_CANCELED)
-                finish()
+                if (anyFieldsFilled()) {
+                    showDialog(
+                        message = "작성 중인 내용이 있습니다.\n정말 나가시겠습니까?",
+                        confirmText = "나가기",
+                        cancelText = "작성하기",
+                        onCancel = { return@showDialog },
+                        onSuccess = { cancelAddContact() },
+                    )
+                } else {
+                    cancelAddContact()
+                }
             }
 
             R.id.buttonSave -> {
@@ -78,6 +95,36 @@ class RegisterActivity : BaseActivity() {
             else -> {}
         }
     }
+
+    private fun cancelAddContact() {
+        val cancelToast = Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT)
+        cancelToast.show()
+        setResult(Activity.RESULT_CANCELED)
+        finish()
+    }
+
+    private fun anyFieldsFilled(): Boolean =
+        textInputName.text
+            .toString()
+            .trim()
+            .isNotEmpty() ||
+            textInputPhone.text
+                .toString()
+                .trim()
+                .isNotEmpty() ||
+            textInputMail.text
+                .toString()
+                .trim()
+                .isNotEmpty() ||
+            textInputBirthday.text
+                .toString()
+                .trim()
+                .isNotEmpty() ||
+            genderRadioGroup.checkedRadioButtonId != -1 ||
+            textInputMemo.text
+                .toString()
+                .trim()
+                .isNotEmpty()
 
     private fun areRequiredFieldsFilled(): Boolean {
         val name = textInputName.text.toString().trim()
--- app/src/main/java/devcon/core/BaseActivity.kt
+++ app/src/main/java/devcon/core/BaseActivity.kt
@@ -2,6 +2,7 @@ package devcon.core
 
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity :
@@ -11,5 +12,30 @@ abstract class BaseActivity :
 
     fun showToast(text: String) {
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+
+    fun showDialog(
+        title: String? = null,
+        message: String? = null,
+        confirmText: String? = null,
+        cancelText: String? = null,
+        onSuccess: (() -> Unit)? = null,
+        onCancel: (() -> Unit)? = null,
+    ) {
+        AlertDialog
+            .Builder(this)
+            .setMessage(message)
+            .apply {
+                title?.let { setTitle(title) }
+            }.apply {
+                message?.let { setMessage(message) }
+            }.setPositiveButton(
+                confirmText ?: "ok",
+            ) { _, _ -> onSuccess?.invoke() }
+            .setNegativeButton(
+                cancelText ?: "cancel",
+            ) { _, _ -> onCancel?.invoke() }
+            .create()
+            .show()
     }
 }
