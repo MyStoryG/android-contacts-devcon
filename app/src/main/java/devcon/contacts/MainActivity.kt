c35c72fa85071876e705c26f873afd2cb383f0bf
Kim-deokhyeon <kkkddh9965@daum.net>
2025-03-15 22:02:09 +0900

    [Feat] 연락처 저장 이후 처리 - 연락처 저장 이후, 화면 업데이트 처리 추가 - 연락처 노출을 위한 layout 작성

--- app/src/main/java/devcon/contacts/MainActivity.kt
+++ app/src/main/java/devcon/contacts/MainActivity.kt
@@ -2,10 +2,15 @@ package devcon.contacts
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import devcon.core.BaseActivity
@@ -14,14 +19,24 @@ import devcon.learn.contacts.R
 
 class MainActivity : BaseActivity() {
     private lateinit var buttonAddContact: Button
+    private lateinit var contactDataLayout: LinearLayout
+    private lateinit var contactScrollView: ScrollView
+    private lateinit var emptyContactView: TextView
+
     private lateinit var contactAddResultLauncher: ActivityResultLauncher<Intent>
+
+    private val colorList = listOf(Color.BLUE, Color.RED, Color.RED, Color.GREEN, Color.LTGRAY)
+
+    private var contactList: MutableList<Contact> = mutableListOf()
 
     private fun setResultContactAdd() {
         contactAddResultLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 if (result.resultCode == Activity.RESULT_OK) {
-                    val contact = result.data?.getParcelableExtra<Contact>("CONTACT")
-                    Log.v("asdf", "contact = $contact")
+                    result.data?.getParcelableExtra<Contact>("CONTACT")?.let {
+                        contactList.add(it)
+                        updateUI()
+                    }
                 }
             }
     }
@@ -35,7 +50,11 @@ class MainActivity : BaseActivity() {
 
     override fun initializeView() {
         buttonAddContact =
-            findViewById<Button?>(R.id.buttonAddContact).also { it.setOnClickListener(this) }
+            findViewById<Button>(R.id.buttonAddContact).also { it.setOnClickListener(this) }
+
+        emptyContactView = findViewById(R.id.emptyContactView)
+        contactDataLayout = findViewById(R.id.contactDataLayout)
+        contactScrollView = findViewById(R.id.contactScrollView)
     }
 
     override fun onClick(v: View?) {
@@ -52,5 +71,41 @@ class MainActivity : BaseActivity() {
 
             else -> {}
         }
+    }
+
+    private fun updateUI() {
+        if (contactList.isEmpty()) {
+            emptyContactView.visibility = View.VISIBLE
+            contactScrollView.visibility = View.GONE
+        } else {
+            emptyContactView.visibility = View.GONE
+            contactScrollView.visibility = View.VISIBLE
+        }
+
+        contactDataLayout.removeAllViews()
+
+        for ((index, contact) in contactList.withIndex()) {
+            contactDataLayout.addView(makeContactView(contact, index))
+        }
+    }
+
+    private fun makeContactView(
+        contact: Contact,
+        index: Int,
+    ): View? {
+        val inflater = LayoutInflater.from(this)
+
+        val contactView = inflater.inflate(R.layout.user_contact, contactDataLayout, false)
+        val nameFirstText = contactView.findViewById<TextView>(R.id.nameFirstText)
+        val userPhoneNumber = contactView.findViewById<TextView>(R.id.userPhoneNumber)
+
+        val drawable = nameFirstText.background as? GradientDrawable
+        drawable?.setColor(colorList[index % 5])
+
+        // 이름의 첫 글자와 유저 번호 설정
+        nameFirstText.text = contact.name.first().toString()
+        userPhoneNumber.text = contact.phoneNumber
+
+        return contactView
     }
 }
