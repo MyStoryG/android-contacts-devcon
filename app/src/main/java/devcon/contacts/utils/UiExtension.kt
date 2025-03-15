432bac0f2d605639390d9c856c0f72446dc8b7a1
sungje365 <dtw2813@gmail.com>
2025-03-16 03:15:00 +0900

    feat: View.showSoftKeyboard 확장 함수 추가

--- app/src/main/java/devcon/contacts/utils/UiExtension.kt
+++ app/src/main/java/devcon/contacts/utils/UiExtension.kt
@@ -1,6 +1,8 @@
 package devcon.contacts.utils
 
 import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.annotation.StringRes
 
@@ -10,4 +12,11 @@ fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
 
 fun Context.showToast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, getString(resId), duration).show()
+}
+
+fun View.showSoftKeyboard() {
+    if (requestFocus()) {
+        val inputMethodManager = context.getSystemService(InputMethodManager::class.java)
+        inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+    }
 }
